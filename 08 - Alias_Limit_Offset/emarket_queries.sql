5822d5e8ebd1ac31cb2171f11687630f731f3925
FrancoRossiDs <francorosssic@gmail.com>
2023-11-08 17:43:42 -0300

    Subo resolución de la clase 8

--- 08 - Alias_Limit_Offset/emarket_queries.sql
+++ 08 - Alias_Limit_Offset/emarket_queries.sql
@@ -132,6 +132,52 @@ o que tengan un título que empiece con “Sales”.*/
 SELECT * 
 FROM clientes
 WHERE Pais IN ("Brazil", "MeExico") or Titulo LIKE "Sales%"; 
+/*2 Obtener un listado de todos los clientes que pertenecen a una compañía que
+empiece con la letra "A"*/
+SELECT *
+FROM clientes
+WHERE Compania LIKE "A%";
+
+/*3 Obtener un listado con los datos: Ciudad, Contacto y renombrarlo como Apellido y
+Nombre, Titulo y renombrarlo como Puesto, de todos los clientes que sean de la
+ciudad "Madrid".
+*/
+SELECT Ciudad,
+       Contacto AS "Apellido y Nombre",
+       Titulo AS "Puesto"
+FROM clientes
+WHERE Ciudad = 'Madrid';
+
+
+/*4 Obtener un listado de todas las facturas con ID entre 10000 y 10500*/
+SELECT *
+FROM facturas
+WHERE FacturaID BETWEEN 10000 AND 10500;
+
+/*5 Obtener un listado de todas las facturas con ID entre 10000 y 10500 o de los clientes
+con ID que empiecen con la letra “B”.*/
+SELECT *
+FROM facturas
+WHERE FacturaID BETWEEN 10000 AND 10500 or ClienteID LIKE "B%";
+
+#6 ¿Existen facturas que la ciudad de envío sea “Vancouver” o que utilicen el correo 3?
+SELECT *
+FROM facturas
+WHERE CiudadEnvio="Vancouver";
+
+#7 ¿Cuál es el ID de empleado de “Buchanan”?
+SELECT EmpleadoID
+FROM empleados
+WHERE Apellido="Buchanan";
+
+/*8 ¿Existen facturas con EmpleadoID del empleado del ejercicio anterior? (No
+relacionar, sino verificar que existan facturas)
+*/
+SELECT FechaFactura
+FROM facturas
+WHERE ClienteID=5;
+
+
 
 
 
